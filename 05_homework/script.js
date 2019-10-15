4e8f0244461e693d2cda1160db0fb1bebb81094e
Igor Valnev <dx89@mail.ru>
2019-10-15 18:14:11 +0300

    edit: hw 5 add answer

--- 05_homework/script.js
+++ 05_homework/script.js
@@ -69,39 +69,95 @@
     const btn7 = document.querySelector('.btn7');
     const div1 = document.querySelector('.div1');
 
+    function aaa() {
+        alert(document.querySelector('.inp5').value);
+    }
+
     btn7.onclick = () => {
-        div1.innerHTML = '<input type="text" class="inp5"><button class="btn8">Show value</button>';
-        btn8.onclick = () => {
-            alert(document.querySelector('.inp5').value);
-        };
+        div1.innerHTML = '<input type="text" class="inp5"><button class="btn8" onclick="aaa()">Show value</button>';
     };
 
-
-
-
 // task 9 --------------------
-    
+    const btn8 = document.querySelector('.btn8');
+    const inp6 = document.querySelector('.inp6');
+
+    btn8.onclick = () => {
+        if(inp6.checked) {
+            alert(inp6.value);
+        } else {
+            alert(false);
+        }
+    };
 
 // task 10 --------------------
-    
+    const btn9 = document.querySelector('.btn9');
+    const inp7 = document.querySelector('.inp7');
+    const div2 = document.querySelector('.div2');
+
+    btn9.onclick = () => {
+        div2.style.backgroundColor = inp7.value;
+    };
 
 // task 11 --------------------
-    
+    const btn10 = document.querySelector('.btn10');
+    const inp8 = document.querySelector('.inp8');
+    const inp9 = document.querySelector('.inp9');
+
+    btn10.onclick = () => {
+        inp9.value = inp8.value;
+    };
     
 // task 12 --------------------
-    
+    const btn11 = document.querySelector('.btn11');
+    const inp10 = document.querySelector('.inp10');
+    const sp4 = document.querySelector('.sp4');
+
+    btn11.onclick = () => {
+        sp4.innerHTML = inp10.value;
+    };
     
 // task 13 --------------------
-    
+    const btn12 = document.querySelector('.btn12');
+    const inp11 = document.querySelector('.inp11');
+    const sp5 = document.querySelector('.sp5');
+    const sp6 = document.querySelector('.sp6');
+
+    btn12.onclick = () => {
+        sp5.innerHTML = inp11.value;
+    };
+
+    inp11.oninput = () => {
+        sp6.innerHTML = inp11.value;
+    };
 
 // task 14 --------------------
-    
+    const btn13 = document.querySelector('.btn13');
+    const sp7 = document.querySelector('.sp7');
+    const ta1 = document.querySelector('#ta1');
+
+    btn13.onclick = () => {
+        sp7.innerHTML = ta1.value;
+    };
 
 // task 15 --------------------
-    
+    const btn14 = document.querySelector('.btn14');
+    const sp8 = document.querySelector('.sp8');
+    const ta2 = document.querySelector('#ta2');
+    const inp12 = document.querySelector('.inp12');
+
+    btn14.onclick = () => {
+        ta2.value = inp12.value;
+        sp8.innerHTML = ta2.value;
+    };
     
 // task 16 --------------------
+    const btn15 = document.querySelector('.btn15');
+    const sp9 = document.querySelector('.sp9');
+    const sel1 = document.querySelector('#sel1');
     
+    btn15.onclick = () => {
+        sp9.innerHTML = sel1.value;
+    };
 
 // task 17 --------------------
    
